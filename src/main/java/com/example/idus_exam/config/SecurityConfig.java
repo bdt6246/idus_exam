f14a5c5102c3387abc1cf6ccbf42edab0a57a0d4
bdt6246 <a01094079599@gmail.com>
2025-02-11 11:03:12 +0900

    [feat]각 url 접근 설정 변경

--- src/main/java/com/example/idus_exam/config/SecurityConfig.java
+++ src/main/java/com/example/idus_exam/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.idus_exam.config;
 
+import com.example.idus_exam.config.filter.JwtFilter;
+import com.example.idus_exam.config.filter.LoginFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,35 +18,33 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-    private final AuthenticationConfiguration configuration;
+  private final AuthenticationConfiguration configuration;
 
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-//    @Bean
-//    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//        http.csrf(AbstractHttpConfigurer::disable);
-//        http.httpBasic(AbstractHttpConfigurer::disable);
-//        http.formLogin(AbstractHttpConfigurer::disable);
-//
-//        http.authorizeHttpRequests(
-//                (auth) -> auth
-//                        .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
-//                        .permitAll()
-//                        .requestMatchers("/course/register").hasRole("INSTRUCTOR")
-//                        .requestMatchers("/login", "/member/signup", "/member/instructor/signup", "/member/verify").permitAll()
-//                        .requestMatchers("/course/list", "/course/*", "/member/verify").permitAll()
-//                        .anyRequest().authenticated()
-//        );
-//
-//        // 기존에 사용자한테 설정하도록 한 쿠키(JSESSIONID)를 사용하지 않도록 하는 설정
-//        http.sessionManagement(AbstractHttpConfigurer::disable);
-//
-//        http.addFilterAt(new LoginFilter(configuration.getAuthenticationManager()), UsernamePasswordAuthenticationFilter.class);
-//        http.addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
-//        return http.build();
-//    }
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable);
+    http.httpBasic(AbstractHttpConfigurer::disable);
+    http.formLogin(AbstractHttpConfigurer::disable);
+
+    http.authorizeHttpRequests(
+        (auth) -> auth
+            .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
+            .permitAll()
+            .requestMatchers("/login", "/user/signup", "/user/verify").permitAll()
+            .requestMatchers("/user/list", "/user/detail", "/order/list").hasRole("USER")
+            .anyRequest().authenticated()
+    );
+
+    http.sessionManagement(AbstractHttpConfigurer::disable);
+
+    http.addFilterAt(new LoginFilter(configuration.getAuthenticationManager()), UsernamePasswordAuthenticationFilter.class);
+    http.addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
 }
